97acef147c1033b68655754e16e76a595fd72799
TheRake66 <65202670+TheRake66@users.noreply.github.com>
2024-02-22 05:18:12 +0100

    assembly

--- Pointeur/Properties/AssemblyInfo.cs
+++ Pointeur/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Copie une position du pointeur sur l'écran dans le presse-papiers.")]
+[assembly: AssemblyTitle("Pointeur")]
 [assembly: AssemblyDescription("Copie une position du pointeur sur l'écran dans le presse-papiers.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thibault BUSTOS (TheRake66)")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]

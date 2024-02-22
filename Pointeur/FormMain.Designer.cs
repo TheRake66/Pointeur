6c03aeba119fb15c8f9ccb69baeda145897f15dd
TheRake66 <65202670+TheRake66@users.noreply.github.com>
2024-02-22 18:44:58 +0100

    remove white flash

--- Pointeur/FormMain.Designer.cs
+++ Pointeur/FormMain.Designer.cs
@@ -36,7 +36,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1, 1);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
